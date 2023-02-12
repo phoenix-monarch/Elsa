d6f08fba21ca38281f189a27041ff529ff518cbe
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-02-12 20:33:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -113,7 +113,7 @@ async def pm_text(bot, message):
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    await message.reply_text("<b>Your message has been sent to my moderators !</b>")
+    await message.reply_text("<b>Your message has been sent to my moderators !If you want any movie then join this group @moviez_community .</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#PM_MSG\n\nName : {user}\n\nID : {user_id}\n\nMessage : {content}</b>"
